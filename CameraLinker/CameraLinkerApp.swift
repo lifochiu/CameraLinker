e3a376ab41f4044df243aac82fdf3a1e335398de
lifochiu <lifochiu.ca@gmail.com>
2026-03-07 13:23:20 -0500

    Initial Commit

---
+++ CameraLinker/CameraLinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraLinkerApp.swift
+//  CameraLinker
+//
+//  Created by Li Feng Chiu on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct CameraLinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
